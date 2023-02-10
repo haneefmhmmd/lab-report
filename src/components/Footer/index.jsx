63d855884f856f68783d4cb91d4f7bf70d3480fa
haneefmhmmd <haneefmhmmd@gmail.com>
2023-02-09 20:02:46 -0500

    view report as pdf functionality completed

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,19 +1,26 @@
-import Button, { ButtonLabel } from "../Button"
-import Flexbox from "../FlexBox"
+import Button, { ButtonLabel } from "../Button";
+import Flexbox from "../FlexBox";
 
-export default function Footer({ addBtnClickHandler }) {
-    return (
-        <footer className="footer">
-            <Flexbox align="center" justify="end" className="container">
-                <Button variant="secondary" className="ml-auto" >
-                    <ButtonLabel label="Continue" />
-                </Button>
-                <Button style={{ "--ml": 5 }} onClick={() => { addBtnClickHandler() }}>
-                    <ButtonLabel label="Add Test" />
-                </Button>
-
-            </Flexbox>
-        </footer>
-    )
+export default function Footer({ addBtnClickHandler, continueBtnHandler }) {
+  return (
+    <footer className="footer" id="footer">
+      <Flexbox align="center" justify="end" className="container">
+        <Button
+          variant="secondary"
+          className="ml-auto"
+          onClick={() => continueBtnHandler()}
+        >
+          <ButtonLabel label="Continue" />
+        </Button>
+        <Button
+          style={{ "--ml": 5 }}
+          onClick={() => {
+            addBtnClickHandler();
+          }}
+        >
+          <ButtonLabel label="Add Test" />
+        </Button>
+      </Flexbox>
+    </footer>
+  );
 }
-
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import styles from './Header.module.css'
+import React from "react";
+import styles from "./Header.module.css";
 export default function Header({ title }) {
-    return (
-        <header className={styles.header}>
-            <div className='container'>
-                <h1>{title}</h1>
-            </div>
-        </header>
-    )
+  return (
+    <header className={styles.header} id="header">
+      <div className="container">
+        <h1>{title}</h1>
+      </div>
+    </header>
+  );
 }
